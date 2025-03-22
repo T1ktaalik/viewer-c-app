dd2d97e3ded3f3d6759e906c8377f972269f2a80
Sergey <88627980+T1ktaalik@users.noreply.github.com>
2025-03-22 12:29:57 +0300

    22-3-2025-1

--- react-app-v1/app/viewer/viewer.tsx
+++ react-app-v1/app/viewer/viewer.tsx
@@ -1,7 +1,16 @@
 export function Viewer() {
+    const viewer
     return (
         <>
-        <div>VIEWER</div>
+        <div>
+            <div id="explorer"></div>
+            <div id="inspector"></div>
+            <div id="toolbar"></div>
+            <div id="viewer">
+                <canvas id="viewerCanvas"></canvas>
+                <canvas id="navCube"></canvas>
+            </div>
+        </div>
         </>
     )
 }
